b54f39973c995c541869656abcb6a9f66cf8552a
varnerlab <jdv27@cornell.edu>
2019-02-21 10:59:40 -0500

    Working on exports

--- src/flux/Export.jl
+++ src/flux/Export.jl
@@ -1,4 +1,4 @@
-function write_reactions_to_file(data_dictionary::Dict{String,Any}, path_to_reaction_file::String)
+function write_model_reactions_to_file(data_dictionary::Dict{String,Any}, path_to_reaction_file::String)
 
     reaction_list = data_dictionary["list_of_chemical_reaction_strings"]
     writedlm(path_to_reaction_file,reaction_list)
