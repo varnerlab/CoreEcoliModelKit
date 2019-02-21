8bbe10ad247858ddb01c30ca5e77bcb55caa3880
varnerlab <jdv27@cornell.edu>
2019-02-21 10:33:11 -0500

    Fixed a bug in Data

--- src/CoreEcoliModel.jl
+++ src/CoreEcoliModel.jl
@@ -14,6 +14,9 @@ export generate_default_data_dictionary
 export constrain_measured_fluxes
 export optimize_specific_growth_rate
 
+# Export utility methods -
+export write_model_reactions_to_file
+
 # export types -
 export VLOptimalFluxResult
 
